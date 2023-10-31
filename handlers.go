46fa14d2254376e694a20ce4636a707f6a49c27e
pkdc <pkdchoy@gmail.com>
2023-10-31 11:47:18 +0000

    added new users and interactions, ready to deploy

--- handlers.go
+++ handlers.go
@@ -33,13 +33,8 @@ func WriteHttpHeader(jsonResp []byte, w http.ResponseWriter) {
 	w.Write(jsonResp)
 }
 
-// func EnableCors(w *http.ResponseWriter) {
-// 	// origin := r.Header.Get("Origin")
-// 	// (*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-// 	(*w).Header().Set("Access-Control-Allow-Origin", "https://notfacebook.netlify.app")
-// 	// (*w).Header().Set("Access-Control-Allow-Origin", origin)
-// 	// (*w).Header().Set("Access-Control-Allow-Origin", "*")
-// 	// (*w).Header().Set("Access-Control-Allow-Origin", "https://cors-anywhere.herokuapp.com/https://notfacebook.netlify.app")
+// func // EnableCors(w *http.ResponseWriter) {
+// 	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 // 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 // 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
 // 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
